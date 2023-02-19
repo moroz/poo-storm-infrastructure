4505d23f56e8fa5f6a72a7e8eb7a5d4fd5cda0d7
Karol Moroz <karol@moroz.dev>
2023-02-19 04:25:02 +0100

    Create a role for Lambda

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "${var.project_name}-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Effect = "Allow"
+      }
+    ]
+  })
+}
