884640ea98c456100238640781f1da8a76d13837
Karol Moroz <karol@moroz.dev>
2023-02-19 14:41:28 +0100

    Deploy lambda

---
+++ lambda.tf
@@ -0,0 +1,13 @@
+resource "aws_lambda_function" "_" {
+  filename         = "./lambda/main.zip"
+  function_name    = "poo-storm-comment-api"
+  role             = aws_iam_role.iam_for_lambda.arn
+  handler          = "main"
+  runtime          = "go1.x"
+  source_code_hash = filebase64sha256("./lambda/main.zip")
+}
+
+resource "aws_lambda_function_url" "api" {
+  function_name      = aws_lambda_function._.function_name
+  authorization_type = "NONE"
+}
